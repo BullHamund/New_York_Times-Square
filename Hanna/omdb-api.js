bee31e8e4effbab1d3ae2295dd5c44664c031ada
Mitu Fikir <hannatefera2@gmail.com>
2019-08-04 16:10:29 -0700

    create function that returns omdbi id

---
+++ Hanna/omdb-api.js
@@ -0,0 +1,17 @@
+var omdbId = "";
+
+function findMovieId(str) {
+    //var movie = $(this).attr("data-name");
+    var queryURL = "https://www.omdbapi.com/?t=" + str + "&apikey=trilogy";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        omdbId = response.imdbID;
+
+
+    });
+
+
+}
+findMovieId("The Godfather");
