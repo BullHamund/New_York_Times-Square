69512c560db13cad068d24da5172ae076fa3f21d
Mercyfulsin <aruiz858@gmail.com>
2019-08-04 18:20:54 -0700

    Searching for a movie hits three API's. One grabs your IP, the other grabs IMDB ID based on query, and the last one combines both of those to find a stream for the movie

---
+++ AndresWork/videospiderApi.js
@@ -0,0 +1,40 @@
+var ticketUrl = "";
+var videoUrl = "";
+var ticketID = "";
+
+$(document).ready(function(){
+    $("#search").click(function(){
+        var query = $("#input").val();
+        console.log("hello");
+        findMovieId(query);
+    });
+});
+
+function updateUrl(){
+    ticketUrl = `https://cors-anywhere.herokuapp.com/https://videospider.in/getticket.php?key=fht9j7i4vSwRR9Ck&secret_key=a8v52mbfjd2enkcqq6pf943muhpl5t&video_id=${omdbId}&ip=${theIP}`;
+    console.log(ticketUrl);
+    getTicket();
+};
+
+function getTicket(){
+    $.ajax({
+        url: ticketUrl,
+        method: 'GET'
+    }).done(function(content){
+        console.log(content);
+        ticketID = content;
+        videoUrl = `https://cors-anywhere.herokuapp.com/https://videospider.stream/getvideo?key=fht9j7i4vSwRR9Ck&video_id=${omdbId}&ticket=${ticketID}`;
+        getVideo();
+    });
+}
+
+function getVideo(){
+    $.ajax({
+        url: videoUrl,
+        method: 'GET'
+    }).done(function(content){
+        console.log(content);
+        var newWindow = window.open();
+        newWindow.document.write(content);
+    });
+}
