fcfbf59f9c30f8096ef9735b78ca7a406e879e69
Mercyfulsin <aruiz858@gmail.com>
2019-08-06 20:34:33 -0700

    Added a check for adblock

--- AndresWork/smartipApi.js
+++ AndresWork/smartipApi.js
@@ -17,5 +17,7 @@ function grabUserIp() {
             theIP = reply;
         }
         console.log(theIP);
+    }).fail(function (e) {
+        document.write("<h1>Please disable adblock and refresh for this page as we need to grab your IP.</h1>");
     });
 }
