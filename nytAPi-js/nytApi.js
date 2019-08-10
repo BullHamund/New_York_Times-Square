291511f1d39980921088956b0e393c21ab75e701
Bill Hammond <willmhammond@gmail.com>
2019-08-10 14:20:43 -0700

    used Lodash to sort NYT ajax call reviews by date in nytApi.js file

--- nytAPi-js/nytApi.js
+++ nytAPi-js/nytApi.js
@@ -1,51 +1,58 @@
-/// SETUP VARIABLES
-// ***************************************
-var authKey = "DqdmALlvW4YGS4KU7Mj87FScRXA4cNSa";
+var apiKey = "DqdmALlvW4YGS4KU7Mj87FScRXA4cNSa";
+var apiUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + keyword + "&api-key=" + apiKey;
+var resultArray;
+var keyword = "";
 
-// Search parameters
-var queryTerm = "";
-var numResults = 0;
 
-// URL
-var queryURLBase = "http://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + authKey;
 
-// Variable to track the number of articles
-var articelCounter = 0;
+$(document).ready(function() {
+    $("#run-search").on("click", function(event) {
+        event.preventDefault();
 
-// FUNCTIONS
-// *****************************************
+        $("#display-review").empty();
+        keyword = $("#movie-search").val();
 
-function runQuery(numArticles, queryURL) {
-    console.log(queryURL);
+        callAjax();
+        $("#movie-search").val("");
+
+    });
+});
+
+function callAjax() {
+    apiUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + keyword +
+        "&api-key=" + apiKey;
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (NYTData) {
-        for (var i= 0; i < NYTData.results.length;i++){
-        console.log(NYTData.results[i].display_title);
-        console.log(NYTData.results[i].mpaa_rating);
-        console.log(NYTData.results[i].summary_short);
+        url: apiUrl,
+        method: 'GET'
+    }).done(function(response) {
+        var dataToArray =_.values(response.results);
+        var results = response.results;
+        var organizedResults = _.sortBy(dataToArray, ["publication_date"]).reverse()
+        console.log(organizedResults);
+        for (var i = 0; i < organizedResults.length; i++) {
+            
+            var resultDiv = $("<div class='card py-2 pl-2 my-2'>");
+
+            var titleDiv = $("<h4>").text(organizedResults[i].display_title);
+            var subtitle = $("<h6 >").text(organizedResults[i].headline);
+            var reviewSummary = $("<p>").text(organizedResults[i].summary_short);
+            var detail = $(`<a class='col-md-3 btn btn-info' href=${organizedResults[i].link.url} target="_blank" role='button'>Review Detail</a> <br>`);
+            resultDiv.append(titleDiv, subtitle, reviewSummary, detail);
+            resultDiv.append(`<button class="col-md-3 btn-info detail" data-choice="${i}">` + "Streaming" + "<br>");
+            $("#display-review").append(resultDiv);
+
+            $(".detail").on("click", function() {
+                var index = $(this).attr('data-choice');
+                //var jsonToSave = JSON.stringify($(this).attr('data-choice'))
+                localStorage.setItem("summary_short", organizedResults[index].summary_short);
+                localStorage.setItem("title", organizedResults[index].display_title);
+                localStorage.setItem("subtitle", organizedResults[index].headline);
+                localStorage.setItem("nylink", organizedResults[index].link.url);
+                document.location.href = "detail.html";
+            });
+
+
+
         }
-    })
+    });
 }
-
-// MAIN PROCESSES
-// ============================================
-
-$("#run-search").on("click", function() {
-
-    // Get the search term
-    queryTerm = $("#movieSearch").val().trim();
-
-    // Add in search term
-    var newURL = queryURLBase + `&query=` + queryTerm;
-
-    // Add in the Number of Records
-    // var numResults = $("#numRecords").val();
-
-    // Send the Ajax Call the newly assembled URL and add numResults after html updated
-    runQuery(10, newURL);
-
-    return false;
-})
-
